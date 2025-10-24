c6c2b201d4887a60c155b67d5b81b9316a58182b
Dev-HazemHefny <hefnyhazem531@gmail.com>
2025-10-24 16:57:08 +0300

    id-verification

--- route/user.route.js
+++ route/user.route.js
@@ -1,11 +1,19 @@
 import { handleValidationErrors } from "../Validators/handleValidationErrors.js";
 import { signupValidation } from "../Validators/signupValidations.js";
-import { confirmEmail, signin, signup } from "../controller/user.controller.js";
+import { confirmEmail, filterUsersByStatus, getUserProfile, signin, signup, switchRole, uploadIdentity, verifyIdentity } from "../controller/user.controller.js";
 import { isEmailExists } from "../middlewares/isEmailExists.js";
+import { auth } from "../middlewares/is_Auth.js";
+import { admin } from "../middlewares/is_Admin.js";
 import express from "express";
+import upload from "../middlewares/identity_cards.js";
 
  const userRouter = express.Router()
 userRouter.post('/signup', signupValidation ,handleValidationErrors, isEmailExists, signup)
 userRouter.post('/signin',signin),
 userRouter.get("/verify/:token", confirmEmail);
+userRouter.patch("/upload-id", auth, upload.single("identityImageUrl"), uploadIdentity);
+userRouter.patch("/switch-role", auth, switchRole);
+userRouter.patch("/verify/:userId", auth, admin, verifyIdentity);
+userRouter.get("/filter", auth, admin, filterUsersByStatus)
+userRouter.get("/profile/:id", auth, getUserProfile)
 export default userRouter
