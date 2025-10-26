bfda57a42fbc378c73c60459e6b2070fac5d4c95
Omar Ali <omarh9392@gmail.com>
2025-10-26 16:51:11 +0300

    cors lib

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ import reservationRouter from "./route/reservaiton.route.js";
 import messageRouter from "./route/message.route.js";
 import conversationRouter from "./route/conversation.route.js";
 import reviewRouter from "./route/Review.route.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 export default app;
 dbConnection;
 app.use('/user',userRouter)
